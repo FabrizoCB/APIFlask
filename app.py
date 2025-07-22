cdf493991e6b3a3827e2a05df9b7ec588db9167f
Fabrizio <castrobarrientosfabriziojamed@gmail.com>
2025-07-22 16:46:51 -0500

    Proyecto base

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from controller.PersonController import PersonBp
+
+app = Flask(__name__)
+
+app.register_blueprint(PersonBp)
+
+@app.route("/")
+def index(): 
+    return "Aplicacion funcionando", 200
+
+@app.route("/health")
+def health():
+    return "Verificacion aplicacion exitosa.", 200
+
+if __name__== "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
